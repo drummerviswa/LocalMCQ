8705483f046f95417a729ad29c5434df8fd70818
Viswanathan P <drummerviswa@gmail.com>
2026-02-16 07:21:26 +0530

    Feat: Ready to publish

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL: process.env.NEXT_PUBLIC_BASE_URL || "https://local-mcq.vercel.app/api",
-    baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000/api",
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL || "https://local-mcq.vercel.app/api",
+    // baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000/api",
     withCredentials: true
 })
 
